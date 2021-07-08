938010dfcd6a0a9d8b1d183b5c4ec88c16e62b63
Ryan Warner <warner.ryan@gmail.com>
2021-07-08 15:32:45 -0400

    Remove unneeded div

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,9 @@ import PageHome from 'components/PageHome'
 
 export default function Home (): ReactElement {
   return (
-    <div>
+    <>
       <Seo />
       <PageHome />
-    </div>
+    </>
   )
 }
