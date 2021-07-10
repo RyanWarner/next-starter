ca56d7e8053f870ab513590b4007b80796529b55
Xander Low <alexanderjameslow@gmail.com>
2021-07-10 11:35:27 -0400

    removed: fc from container

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,17 +1,15 @@
-import { ReactElement, FC } from 'react'
+import { ReactElement } from 'react'
 
 import * as S from './styles'
 
 interface ContainerProps {
-  children?: ReactElement | ReactElement[],
+  children?: ReactElement | ReactElement[]
   maxWidth?: string
 }
 
 const Container = ({ children, maxWidth }: ContainerProps): ReactElement => {
   return (
-    <S.ContainerComponent maxWidth={maxWidth}>
-      {children}
-    </S.ContainerComponent>
+    <S.ContainerComponent maxWidth={maxWidth}>{children}</S.ContainerComponent>
   )
 }
 
