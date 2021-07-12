2419cace3284bde2f14317318fc9c4e1e31037d2
Xander Low <alexanderjameslow@gmail.com>
2021-07-12 12:18:38 -0400

    changed: removed Seo component in favour of NextSeo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,27 @@ import { ThemeProvider } from 'styled-components'
 
 import { GlobalStyle } from 'styles/global'
 import theme from 'styles/theme'
+import { DefaultSeo } from 'next-seo'
 
 function MyApp ({ Component, pageProps }: AppProps): ReactElement {
   return (
     <ThemeProvider theme={theme}>
+      <DefaultSeo 
+        title="Next Starter"
+        titleTemplate="%s | Next Starter"
+        description="A description for this new site you've created."
+        openGraph={{
+          type: "website",
+          locale: "en_IE",
+          url: "https://www.url.ie/",
+          site_name: "SiteName",
+        }}
+        twitter={{
+          handle: "@handle",
+          site: "@site",
+          cardType: "summary_large_image",
+        }}
+      />
       <GlobalStyle />
       <Component {...pageProps} />
     </ThemeProvider>
