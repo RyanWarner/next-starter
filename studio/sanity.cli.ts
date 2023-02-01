75a13701b28b18d04a9526338e8294dad0bd12ac
Ryan Warner <warner.ryan@gmail.com>
2023-02-01 15:06:18 -0500

    add @sanity/cli as dep

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -1,4 +1,4 @@
-import {defineCliConfig} from '@sanity/cli'
+import {defineCliConfig} from 'sanity/cli'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
