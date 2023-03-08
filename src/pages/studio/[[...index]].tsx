ba7c78f7cfdb0816fe6d75db41f29cc940db218b
Ryan Warner <warner.ryan@gmail.com>
2023-03-07 19:03:52 -0500

    null check sanity config before rendering studio

--- src/pages/studio/[[...index]].tsx
+++ src/pages/studio/[[...index]].tsx
@@ -4,12 +4,16 @@ import { NextStudioHead } from 'next-sanity/studio/head'
 import { StudioLayout, StudioProvider } from 'sanity'
 import config from 'sanity-studio/sanity.config'
 import { createGlobalStyle } from 'styled-components'
+import { Heading } from '@chakra-ui/react'
 
 const GlobalStyle = createGlobalStyle(({ theme }: any) => ({
   html: { backgroundColor: theme.sanity.color.base.bg }
 }))
 
 export default function StudioPage() {
+  if (!config) {
+    return <Heading>Error: Sanity config issue.</Heading>
+  }
   return (
     <>
       <Head>
