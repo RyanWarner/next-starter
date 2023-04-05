f80a8ff6c67a211eb5789cbe3e0308255706dded
Ryan Warner <warner.ryan@gmail.com>
2023-04-05 18:39:00 -0400

    add analytics component to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,13 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Seo } from 'components/Seo'
 import { theme } from 'theme'
+import { Analytics } from 'components/Analytics'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
       <Seo />
+      <Analytics />
       <Component {...pageProps} />
     </ChakraProvider>
   )
